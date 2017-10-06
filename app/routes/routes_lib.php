37ba74261f4993c36390059fc8a68f0f65231bc9
Theo-Drappier <theo.drappier85@gmail.com>
2017-10-06 15:40:06 +0200

    Create routes_lib.php

---
+++ app/routes/routes_lib.php
@@ -0,0 +1,15 @@
+<?php
+
+$app->get('/lib/css/{nomFichier}', function($request, $response, $args){
+  ob_start();
+  require 'lib/css/'.$args['nomFichier'];
+  $view = ob_get_clean();
+  return $view;
+});
+
+$app->get('/lib/js/{nomFichier}', function($request, $response, $args){
+  ob_start();
+  require 'lib/js/'.$args['nomFichier'];
+  $view = ob_get_clean();
+  return $view;
+});
