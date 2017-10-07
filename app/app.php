1045dfff3586e801891eaf551c0ef7fa8a17cfb9
Théo Drappier <theo.drappier85@gmail.com>
2017-10-07 16:52:43 +0200

    Commit class UsersDAO

--- app/app.php
+++ app/app.php
@@ -5,4 +5,4 @@ use RedBeanPHP\R;
 R::setup('mysql:host=localhost;dbname=dip-purple_v0',
          'YOUR_USERNAME', 'YOUR_PASSWORD');
 
-//$services['dao.fields'] = \DIP\dao\FieldsDAO::getInstances();
+$services['dao.users'] = \DIP\dao\UsersDAO::getInstances();
