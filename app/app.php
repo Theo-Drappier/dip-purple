383c7ed9f458410f4ed023f609d0f4cc5f8a7bf8
Théo Drappier <theo.drappier85@gmail.com>
2017-10-07 13:30:55 +0200

    Delete Index

--- app/app.php
+++ app/app.php
@@ -2,7 +2,7 @@
 
 use RedBeanPHP\R;
 
-R::setup('mysql:host=localhost;dbname=hackathyon_test',
+R::setup('mysql:host=localhost;dbname=dip-purple_v0',
          'YOUR_USERNAME', 'YOUR_PASSWORD');
 
-$services['dao.fields'] = \DIP\dao\FieldsDAO::getInstances();
+//$services['dao.fields'] = \DIP\dao\FieldsDAO::getInstances();
