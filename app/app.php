751d500c1584435bc3eacb522bdcc9213385571d
Theo-Drappier <theo.drappier85@gmail.com>
2017-10-07 17:11:16 +0200

    Update app.php

--- app/app.php
+++ app/app.php
@@ -6,3 +6,10 @@ R::setup('mysql:host=localhost;dbname=dip-purple_v0',
          'YOUR_USERNAME', 'YOUR_PASSWORD');
 
 $services['dao.users'] = \DIP\dao\UsersDAO::getInstances();
+$services['dao.appareil'] = \DIP\dao\AppareilDAO::getInstances();
+$services['dao.etat'] = \DIP\dao\EtatDAO::getInstances();
+$services['dao.family'] = \DIP\dao\FamilyDAO::getInstances();
+$services['dao.heurepc'] = \DIP\dao\HeurepcDAO::getInstances();
+$services['dao.home'] = \DIP\dao\HomeDAO::getInstances();
+$services['dao.homepiece'] = \DIP\dao\HomePieceDAO::getInstances();
+$services['dao.piece'] = \DIP\dao\PieceDAO::getInstances();
