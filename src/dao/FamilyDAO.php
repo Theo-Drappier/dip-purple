1affa2daeada60edcea154dec30cd975f2be3506
Théo Drappier <theo.drappier85@gmail.com>
2017-10-07 19:08:28 +0200

    Commit rectif bug FONCTIONNEL

--- src/dao/FamilyDAO.php
+++ src/dao/FamilyDAO.php
@@ -6,17 +6,17 @@ use RedBeanPHP\R;
 
 class FamilyDAO extends dao
 {
-    private function _construct()
+    private function __construct()
     {
         $this->class = 'family';
     }
-    public static function getInstances() 
+    public static function getInstances()
     {
         if(!isset(self::$_instances['family']))
         {
             self::$_instances['family']= new FamilyDAO();
         }
         return self::$_instances['family'];
-     
-    }            
+
+    }
 }
