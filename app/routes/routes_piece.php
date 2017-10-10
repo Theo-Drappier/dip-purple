ecf27d9ab6f1a7a45a28a75c54b9a888514b5264
Théo Drappier <theo.drappier85@gmail.com>
2017-10-10 14:20:40 +0200

    Add Method Calculate Time

--- app/routes/routes_piece.php
+++ app/routes/routes_piece.php
@@ -9,19 +9,20 @@ $app->get('/piece/{idPiece}', function($request, $response, $args) use ($service
         $home = $services['dao.home']->findOneByIdFamily($_SESSION['famille']->id);
         $piece = $services['dao.piece']->findOneById($args['idPiece']);
         $homepieces = $services['dao.homepiece']->findAllByHomePiece($home->id, $piece->id);
-        
+
         $appareils = [];
         $actions = [];
         $etats = [];
         for($i = 0; $i < sizeof($homepieces); $i++)
         {
-            $actions[] = $services['dao.action']->findLastAction($homepieces[$i]->id);
+            $actions[] = $services['dao.action']->findLastActions($homepieces[$i]->id);
             if(empty($actions[$i]))
             {
                 $etats[] = null;
             }
             else {
-                $bareme = $services['dao.bareme']->findOneById($actions[$i]->id_bareme);
+                $heure = $services['dao.action']->getDiffTime($actions[$i]);
+                $bareme = $services['dao.bareme']->findOneById($actions[$i][0]['id_bareme']);
                 $etats[] = $services['dao.etat']->findOneById($bareme->id_etat);
             }
 
