9fa43353d874ff9d9b05dbd8118012c84bfe684e
Théo Drappier <theo.drappier85@gmail.com>
2017-10-11 07:38:55 +0200

    Test

--- views/base/footer.php
+++ views/base/footer.php
@@ -3,5 +3,4 @@
       <b>Version</b> 1.0.0
     </div>
     <strong>DIP' Purple Hackath'yon 2017 <a href="https://adminlte.io">Almsaeed Studio</a>. Projet disponible <a href="https://github.com/Theo-Drappier/dip-purple">ici</a> </strong>
-    reserved.
 </footer>
